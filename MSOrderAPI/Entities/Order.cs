77bfede355b617159d8e5578f7e870b41cc60e95
gurau-anil <gurau.anil@gmail.com>
2025-05-19 23:33:16 -0700

    Order Entity

---
+++ MSOrderAPI/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MSOrderAPI.Entities
+{
+    public class Order
+    {
+        public string OrderId { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
+        public List<OrderItem> Items { get; set; } = new();
+
+        [Range(0, double.MaxValue)]
+        public decimal TotalAmount { get; set; }
+    }
+}
---
+++ MSOrderAPI/Entities/OrderItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MSOrderAPI.Entities
+{
+    public class OrderItem
+    {
+        public string OrderItemId { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal UnitPrice { get; set; }
+
+        public string OrderId { get; set; } = string.Empty;
+    }
+}
