37d211b94b953e49bfd53c3efeda62fbbb4d7651
gurau-anil <gurau.anil@gmail.com>
2025-05-20 22:16:03 -0700

    CartAPI DataContext

---
+++ MSCartAPI/Data/CartDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MSCartAPI.Entities;
+
+namespace MSCartAPI.Data
+{
+    public class CartDbContext : DbContext
+    {
+        public CartDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+    }
+}
